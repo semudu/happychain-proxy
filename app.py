466d4181676a503c8b036342bee60092beba8632
Serhat Durmaz <serhat.md@gmail.com>
2020-02-19 21:26:47 +0300

    team received transactions api added

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-import config
 import requests
 from flask import Flask
+from requests.auth import HTTPBasicAuth
 
+import config
 
 app = Flask(__name__)
+auth = HTTPBasicAuth(config.USERNAME, config.PASSWORD)
+
 
 @app.route("/")
 def hello():
     response = requests.get(config.API_URL)
+    return response.text, response.status_code
+
+
+@app.route("/transaction/received/team/<team_id>")
+def get_received_transactions(team_id):
+    response = requests.get(config.API_URL + "/transaction/received/team/" + team_id, auth=auth)
     return response.text, response.status_code
 
 
